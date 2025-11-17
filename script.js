01b015e18458fc5551d72774670d08ee84a8e290
luisvillada <otrodeluisvillada@gmail.com>
2025-11-17 01:22:01 -0500

    Se sube LAB toDo con Bootstrap

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const textInput = document.getElementById("tareaNueva");
 const botonAgregar = document.getElementById("agregar");
+const botonBorrar = document.getElementById("borrar")
 const lista = document.getElementById("manageDom");
 let contador = 0; 
+
+// Agregar elementos a la 
 botonAgregar.addEventListener('click', () => {
     const texto = textInput.value.trim();
         console.log(texto);
@@ -23,4 +26,21 @@ botonAgregar.addEventListener('click', () => {
         nuevoItem.appendChild(nuevoLabel);
         textInput.value = "";
     }
+});
+
+botonBorrar.addEventListener("click", () => {
+    const checkBoxes = lista.querySelectorAll("input[type='checkbox;']");
+    
+    const marcados = Array.from(checkBoxes).filter(cb => cb.checked);
+    if(marcados.length === 0){
+        alert ("Por favor, selecciona al menos una tarea para borrar.");
+        return;
+    }
+    if(confirm("Borrar  elementos seleccionados?")){
+        marcados.forEach(checkBox => {
+            const li = checkBox.closest("li");  
+            if (li) li.remove();    
+        });
+    }
+     
 });
